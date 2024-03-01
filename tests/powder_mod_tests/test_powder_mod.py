e64d096304a8eace4dc09a664eb83f1c333befbd
Andrew Sazonov <andrew.sazonov@icloud.com>
2024-03-01 11:12:53 +0100

    Revert "Try P n m a"
    This reverts commit a5964ec9a803003dd8fc76fc277a76af0c2620a4.

--- tests/powder_mod_tests/test_powder_mod.py
+++ tests/powder_mod_tests/test_powder_mod.py
@@ -176,7 +176,7 @@ if __name__ == '__main__':
     #print('::::: Y calculated (P m -3 m):', ycalc)
 
     clean_after_compute(STUDY_DICT)
-    study_dict["phases"][0]["SrTiO3"]["_space_group_name_H-M_alt"] = 'P n m a'
+    study_dict["phases"][0]["SrTiO3"]["_space_group_name_H-M_alt"] = 'I 4 3 2'
     _, ycalc = powder_mod.simulation(study_dict)
-    print('::::: Y calculated (P n m a):', ycalc)
+    print('::::: Y calculated (I 4 3 2):', ycalc)
     np.savetxt('ycalc.dat', ycalc, fmt='%12.6f')

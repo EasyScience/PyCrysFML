f4c3614b18d1f9e7f65b3321413d5ce09973cadd
Andrew Sazonov <andrew.sazonov@icloud.com>
2024-07-12 14:18:34 +0200

    add macos and clean up

--- tests/unit_tests/pycfml/test__cfml_metrics__debug.py
+++ tests/unit_tests/pycfml/test__cfml_metrics__debug.py
@@ -8,46 +8,59 @@ import numpy as np
 from numpy.testing import assert_almost_equal
 
 
-from pycrysfml08 import crysfml08lib
-
-print('!!!!! IMPORTING SUCCESS !!!!!')
-
 # Tests
 
-def test__get_u_from_b__directly():
+def test__get_u_from_b__crysfml08lib_f_get_u_from_b():
     try:
         from pycrysfml08 import crysfml08lib
+        nd_b = np.array([1.5, 1.6, 1.7, 1.0, 1.1, 1.2], dtype='f')
+        desired = np.array([0.019, 0.0203, 0.0215, 0.0127, 0.0139, 0.0152], dtype='f')
+        code, message, actual = crysfml08lib.f_get_u_from_b(nd_b)
+        assert code == 0
+        assert message == ''
+        assert_almost_equal(desired, actual, decimal=4, verbose=True)
     except Exception as e:
-        print('::::: Run test__get_u_from_b__directly() :::::')
+        print('::::: Run test__get_u_from_b__crysfml08lib_f_get_u_from_b() :::::')
         print('Error')
         print(e)
         print()
         return
-    nd_b = np.array([1.5, 1.6, 1.7, 1.0, 1.1, 1.2], dtype='f')
-    desired = np.array([0.019, 0.0203, 0.0215, 0.0127, 0.0139, 0.0152], dtype='f')
-    code, message, actual = crysfml08lib.f_get_u_from_b(nd_b)
-    assert code == 0
-    assert message == ''
-    assert_almost_equal(desired, actual, decimal=4, verbose=True)
 
-def test__get_u_from_b__pyapi():
+def test__get_u_from_b__crysfml08lib_get_u_from_b():
+    try:
+        from pycrysfml08 import crysfml08lib
+        nd_b = np.array([1.5, 1.6, 1.7, 1.0, 1.1, 1.2], dtype='f')
+        desired = np.array([0.019, 0.0203, 0.0215, 0.0127, 0.0139, 0.0152], dtype='f')
+        code, message, actual = crysfml08lib.get_u_from_b(nd_b)
+        assert code == 0
+        assert message == ''
+        assert_almost_equal(desired, actual, decimal=4, verbose=True)
+    except Exception as e:
+        print('::::: Run test__get_u_from_b__crysfml08lib_get_u_from_b() :::::')
+        print('Error')
+        print(e)
+        print()
+        return
+
+def test__get_u_from_b__cfml_metrics_get_u_from_b():
     try:
         from pycrysfml08 import cfml_metrics
+        nd_b = np.array([1.5, 1.6, 1.7, 1.0, 1.1, 1.2], dtype='f')
+        desired = np.array([0.019, 0.0203, 0.0215, 0.0127, 0.0139, 0.0152], dtype='f')
+        code, message, actual = cfml_metrics.get_u_from_b(nd_b)
+        assert code == 0
+        assert message == ''
+        assert_almost_equal(desired, actual, decimal=4, verbose=True)
     except Exception as e:
-        print('::::: Run test__get_u_from_b__pyapi() :::::')
+        print('::::: Run test__get_u_from_b__cfml_metrics_get_u_from_b() :::::')
         print('Error')
         print(e)
         print()
         return
-    nd_b = np.array([1.5, 1.6, 1.7, 1.0, 1.1, 1.2], dtype='f')
-    desired = np.array([0.019, 0.0203, 0.0215, 0.0127, 0.0139, 0.0152], dtype='f')
-    code, message, actual = cfml_metrics.get_u_from_b(nd_b)
-    assert code == 0
-    assert message == ''
-    assert_almost_equal(desired, actual, decimal=4, verbose=True)
 
 # Debug
 
 if __name__ == '__main__':
-    test__get_u_from_b__directly()
-    test__get_u_from_b__pyapi()
+    test__get_u_from_b__crysfml08lib_f_get_u_from_b()
+    test__get_u_from_b__crysfml08lib_get_u_from_b()
+    test__get_u_from_b__cfml_metrics_get_u_from_b()

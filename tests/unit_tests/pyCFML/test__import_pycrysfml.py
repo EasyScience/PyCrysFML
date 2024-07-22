1504cca0afd941bcddf1f8c214be022c8f703195
Andrew Sazonov <andrew.sazonov@icloud.com>
2024-07-22 10:09:59 +0200

    Update pycfml import debug

--- tests/unit_tests/pyCFML/test__import_pycrysfml.py
+++ tests/unit_tests/pyCFML/test__import_pycrysfml.py
@@ -4,21 +4,24 @@ import sys
 # Tests
 
 def test__from_pycrysfml_import_crysfml08lib():
+    msg = 'from pycrysfml import crysfml08lib'
     try:
         from pycrysfml import crysfml08lib
+        print(f"::::: Succeeded to '{msg}'")
         assert True
     except Exception as e:
-        print("::::: Failed to 'from pycrysfml import crysfml08lib'")
+        print(f"::::: Failed to '{msg}': {e}")
         print('::::: ERROR:', e)
         #assert False
 
 def test__from_pycrysfml_import_cfml_utilities():
+    msg = 'from pycrysfml import cfml_utilities'
     try:
         from pycrysfml import cfml_utilities
+        print(f"::::: Succeeded to '{msg}'")
         assert True
     except Exception as e:
-        print("::::: Failed to 'from pycrysfml import cfml_utilities'")
-        print('::::: ERROR:', e)
+        print(f"::::: Failed to '{msg}': {e}")
         #assert False
 
 # Debug

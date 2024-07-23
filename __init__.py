c041506e8049f2f7603d73a885753a2da25094ee
Andrew Sazonov <andrew.sazonov@icloud.com>
2024-07-23 14:40:54 +0200

    Add examples, issues, extend tests

--- __init__.py
+++ __init__.py
@@ -1,4 +1,12 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(__file__))
+
+CURRENT_PATH = os.path.dirname(__file__)
+
+
+# Add current path to system path
+sys.path.append(CURRENT_PATH)
+
+# Set en variable CRYSFML_DB to be the path to the Databases directory
+os.environ['CRYSFML_DB'] = os.path.join(CURRENT_PATH, 'Databases')
--- tests/functional_tests/pyCFML/cfml_utilities/powder_pattern_from_json/test__powder_pattern_from_json.py
+++ tests/functional_tests/pyCFML/cfml_utilities/powder_pattern_from_json/test__powder_pattern_from_json.py
@@ -1,19 +1,19 @@
+import argparse
 import copy
 import os
 import numpy as np
 from numpy.testing import assert_almost_equal
-from pycrysfml import crysfml08lib
+
+#import cfml_utilities
 #from pycrysfml import cfml_utilities
+from pycrysfml import crysfml08lib
 
 
-os.environ['CRYSFML_DB'] = os.path.join(os.path.dirname(crysfml08lib.__file__), 'Databases')  # access to Databases/magnetic_data.txt
-
-STUDY_DICT = {
+STUDY_DICT_PM3M = {
   "phases": [
     {
       "SrTiO3": {
-        #"_space_group_name_H-M_alt": "P m -3 m",
-        "_space_group_name_H-M_alt": "P n m a",
+        "_space_group_name_H-M_alt": "P m -3 m",
         "_cell_length_a": 3.9,
         "_cell_length_b": 3.9,
         "_cell_length_c": 3.9,
@@ -73,23 +73,7 @@ STUDY_DICT = {
         "_pd_meas_2theta_offset": 0,
         "_pd_meas_2theta_range_min": 1,
         "_pd_meas_2theta_range_max": 140,
-        "_pd_meas_2theta_range_inc": 0.05,
-        "_phase": [
-          {
-            "_label": "SrTiO3",
-            "_scale": 1
-          }
-        ],
-        "_pd_background": [
-          {
-            "_2theta": 1,
-            "_intensity": 20
-          },
-          {
-            "_2theta": 140,
-            "_intensity": 20
-          }
-        ]
+        "_pd_meas_2theta_range_inc": 0.05
       }
     }
   ]
@@ -100,48 +84,37 @@ STUDY_DICT = {
 def path_to_desired(file_name:str):
     return os.path.join(os.path.dirname(__file__), 'desired', file_name)
 
-def phase_name_by_idx(study_dict:dict, phase_idx:int=0):
-    return list(study_dict['phases'][phase_idx].keys())[phase_idx]
-
-def space_group_by_phase_idx(study_dict:dict, phase_idx:int=0):
-    phase_name = phase_name_by_idx(study_dict, phase_idx)
-    return study_dict['phases'][phase_idx][phase_name]['_space_group_name_H-M_alt']
-
-def set_space_group_by_phase_idx(study_dict:dict, phase_idx:int, space_group:str):
-    phase_name = phase_name_by_idx(study_dict, phase_idx)
-    study_dict['phases'][phase_idx][phase_name]['_space_group_name_H-M_alt'] = space_group
-
 def compute_pattern(study_dict:dict):
+    #_, y = cfml_utilities.powder_pattern_from_json(study_dict)  # returns x and y arrays
     _, y = crysfml08lib.f_powder_pattern_from_json(study_dict)  # returns x and y arrays
     y = y.astype(np.float64)
     return y
 
-#def compute_pattern_2(study_dict:dict):
-#    return cfml_utilities.powder_pattern_from_json(study_dict)
-
 # Tests
 
-def _test__compute_pattern__SrTiO3_Pm3m():
+def test__compute_pattern__SrTiO3_Pm3m():
+    study_dict = copy.deepcopy(STUDY_DICT_PM3M)
+    norm = 120
     _, desired = np.loadtxt(path_to_desired('srtio3-pm3m-pattern_Nebil-ifort.xy'), unpack=True)
     desired = desired - 20.0  # remove background
-    study_dict = copy.deepcopy(STUDY_DICT)
-    set_space_group_by_phase_idx(study_dict, phase_idx=0, space_group='P m -3 m')
-    pattern = compute_pattern(study_dict)
-    actual = pattern.astype(np.float64)
-    # compensate for a 1-element shift in y data between Nebil windows build and my gfortran build
-    #desired = desired[1:]
-    #actual = actual[:-1]
+    desired = np.roll(desired, -1)  # compensate for a 1-element horizontal shift in y data between old Nebil windows build and Andrew current gfortran build
+    desired = desired / norm
+    actual = compute_pattern(study_dict)
+    actual = actual / norm
     assert_almost_equal(desired, actual, decimal=0, verbose=True)
 
 def test__compute_pattern__SrTiO3_Pnma():
-    desired = np.loadtxt(path_to_desired('srtio3-pmmm-pattern_Andrew-ifort.xy'), unpack=True)
+    study_dict = copy.deepcopy(STUDY_DICT_PM3M)
+    study_dict['phases'][0]['SrTiO3']['_space_group_name_H-M_alt'] = 'P n m a'
+    norm = 0.65
+    desired = np.loadtxt(path_to_desired('srtio3-pnma-pattern_Andrew-ifort.y'), unpack=True)
     desired = desired - 20.0  # remove background
-    study_dict = copy.deepcopy(STUDY_DICT)
-    set_space_group_by_phase_idx(study_dict, phase_idx=0, space_group='P n m a')
+    desired = desired / norm
     actual = compute_pattern(study_dict)
+    actual = actual / norm
     assert_almost_equal(desired, actual, decimal=2, verbose=True)
 
 # Debug
 
 if __name__ == '__main__':
-    test__compute_pattern__SrTiO3_Pnma()
+    pass

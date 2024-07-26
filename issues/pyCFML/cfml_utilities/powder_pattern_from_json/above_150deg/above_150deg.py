ab438bcfe79a15750691e3168c3126bd0fc9191b
Andrew Sazonov <andrew.sazonov@icloud.com>
2024-07-26 20:52:20 +0200

    Add one more issue

--- issues/pyCFML/cfml_utilities/powder_pattern_from_json/above_150deg/above_150deg.py
+++ issues/pyCFML/cfml_utilities/powder_pattern_from_json/above_150deg/above_150deg.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from pycrysfml import cfml_utilities
-#from pycrysfml import crysfml08lib
+#import cfml_utilities
 
 
-STUDY_DICT_PM3M = {
+STUDY_DICT = {
   "phases": [
     {
       "SrTiO3": {
@@ -80,18 +80,6 @@ STUDY_DICT_PM3M = {
 
 # Help functions
 
-def parsed_args():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--calc",
-                        default="none",
-                        choices=['none', 'pm3m', 'pnma-then-pm3m'],
-                        type=str.lower,
-                        help="calculation order: only SG Pm-3m, or first SG Pnma and then SG Pm-3m")
-    parser.add_argument("--plot",
-                        action='store_true',
-                        help="plot simulated patterns after previous runs")
-    return parser.parse_args()
-
 def generated_x_array(study_dict:dict):
     experiment = study_dict['experiments'][0]['NPD']
     start = experiment['_pd_meas_2theta_range_min']
@@ -102,54 +90,26 @@ def generated_x_array(study_dict:dict):
 
 def compute_pattern(study_dict:dict):
     _, y = cfml_utilities.powder_pattern_from_json(study_dict)  # returns x and y arrays
-    #_, y = crysfml08lib.f_powder_pattern_from_json(study_dict)  # returns x and y arrays
     y = y.astype(np.float64)
     return y
 
 # Main
 
 if __name__ == '__main__':
-    ARGS = parsed_args()
+    study_dict = copy.deepcopy(STUDY_DICT)
 
-    if ARGS.calc == 'pm3m':
-        study_dict = copy.deepcopy(STUDY_DICT_PM3M)
+    # set upper limit to 140deg
+    study_dict['experiments'][0]['NPD']['_pd_meas_2theta_range_max'] = 150.0
+    x_1 = generated_x_array(study_dict)
+    y_1 = compute_pattern(study_dict)
 
-        y_pm3m = compute_pattern(study_dict)
+    # set upper limit to 160deg
+    study_dict['experiments'][0]['NPD']['_pd_meas_2theta_range_max'] = 160.0
+    x_2 = generated_x_array(study_dict)
+    y_2 = compute_pattern(study_dict)
 
-        file_name = 'y-pm3m_first-calc.txt'
-        np.savetxt(file_name, y_pm3m, fmt='%10.2f')
-
-        sg = study_dict['phases'][0]['SrTiO3']['_space_group_name_H-M_alt']
-        print(f"First calculation for SG '{sg}' saved into '{file_name}'")
-
-    if ARGS.calc == 'pnma-then-pm3m':
-        study_dict = copy.deepcopy(STUDY_DICT_PM3M)
-
-        study_dict['phases'][0]['SrTiO3']['_space_group_name_H-M_alt'] = 'P n m a'
-        y_pnma = compute_pattern(study_dict)
-
-        study_dict['phases'][0]['SrTiO3']['_space_group_name_H-M_alt'] = 'P m -3 m'
-        y_pm3m = compute_pattern(study_dict)
-
-        file_name = 'y-pm3m_second-calc.txt'
-        np.savetxt(file_name, y_pm3m, fmt='%10.2f')
-
-        sg = study_dict['phases'][0]['SrTiO3']['_space_group_name_H-M_alt']
-        print(f"Second calculation for SG '{sg}' saved into '{file_name}'")
-
-    if ARGS.plot:
-        study_dict = copy.deepcopy(STUDY_DICT_PM3M)
-
-        x = generated_x_array(study_dict)
-
-        y_pm3m_1 = np.loadtxt('y-pm3m_first-calc.txt', unpack=True)
-        y_pm3m_2 = np.loadtxt('y-pm3m_second-calc.txt', unpack=True)
-
-        plt.plot(x, y_pm3m_1, '-', linewidth=3)
-        plt.plot(x, y_pm3m_2, '-', linewidth=2)
-        plt.plot(x, y_pm3m_1-y_pm3m_2, '-')
-        #plt.xlim([31, 35])
-
-        plt.legend(["Pm-3m, first calc", "Pm-3m, second calc", "diff"])
-
-        plt.show()
+    # plot results§
+    plt.plot(x_1, y_1, '-', linewidth=3)
+    plt.plot(x_2, y_2, '-', linewidth=2)
+    plt.legend(["2theta_range_max = 150", "2theta_range_max = 160"])
+    plt.show()
